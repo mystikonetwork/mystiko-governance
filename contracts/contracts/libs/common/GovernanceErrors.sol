5f12175d1bf829d35fa8939b1b72e6ab82ff2d5d
ericlurker <96100006+ericlurker@users.noreply.github.com>
2024-03-15 19:22:46 +0800

    update, change MystikoTimelockController

---
+++ contracts/contracts/libs/common/GovernanceErrors.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+library GovernanceErrors {
+  error NotChanged();
+  error OnlyMystikoDAO();
+  error UnauthorizedRole();
+  error InsufficientBalanceForAction();
+  error RollupSizeTooSmall();
+}
