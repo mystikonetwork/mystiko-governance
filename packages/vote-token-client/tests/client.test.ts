22ff09bc6d5a9402fb97504a48f8b9bce51fc47d
ericlurker <96100006+ericlurker@users.noreply.github.com>
2024-06-20 11:34:52 +0800

    update, fix lint error

--- packages/vote-token-client/tests/client.test.ts
+++ packages/vote-token-client/tests/client.test.ts
@@ -1,5 +1,5 @@
 import { test, expect } from '@jest/globals';
-import voteTokenClient, {Client} from "../src";
+import voteTokenClient, { Client } from '../src';
 
 test('test balance', () => {
   expect(voteTokenClient).toBeInstanceOf(Client);
