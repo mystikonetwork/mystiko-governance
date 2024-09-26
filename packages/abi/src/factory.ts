914f2e30dcdac928aabccce7e41192dca1434a35
ericlurker <96100006+ericlurker@users.noreply.github.com>
2024-09-26 17:45:53 +0800

    update, config support monitor governor proposal

--- packages/abi/src/factory.ts
+++ packages/abi/src/factory.ts
@@ -2,13 +2,15 @@ import { providers, Signer } from 'ethers';
 import {
   ERC20,
   ERC20__factory,
+  Governor,
+  Governor__factory,
   MystikoDAOAccessControl,
   MystikoDAOAccessControl__factory,
   MystikoVoteToken,
   MystikoVoteToken__factory,
 } from './typechain/governance';
 
-export type SupportedContractType = ERC20 | MystikoDAOAccessControl | MystikoVoteToken;
+export type SupportedContractType = ERC20 | Governor | MystikoDAOAccessControl | MystikoVoteToken;
 
 export class MystikoGovernanceContractFactory {
   public static connect<T extends SupportedContractType>(
@@ -24,6 +26,24 @@ export class MystikoGovernanceContractFactory {
     }
     if (contractName === 'MystikoVoteToken') {
       return MystikoVoteToken__factory.connect(address, signerOrProvider) as T;
+    }
+    if (contractName === 'Governor') {
+      return Governor__factory.connect(address, signerOrProvider) as T;
+    }
+    throw new Error(`unsupported contract name ${contractName}`);
+  }
+}
+
+export type SupportedOpenZeppelinContractType = Governor;
+
+export class OpenZeppelinGovernanceContractFactory {
+  public static connect<T extends SupportedOpenZeppelinContractType>(
+    contractName: string,
+    address: string,
+    signerOrProvider: Signer | providers.Provider,
+  ): T {
+    if (contractName === 'Governor') {
+      return Governor__factory.connect(address, signerOrProvider) as T;
     }
     throw new Error(`unsupported contract name ${contractName}`);
   }
---
+++ packages/monitor/src/config/config.ts
@@ -0,0 +1,54 @@
+import { Governor, OpenZeppelinGovernanceContractFactory } from '@mystikonetwork/contracts-abi-governance';
+import { providers } from 'ethers';
+
+export type ChainConfig = {
+  chainId: number;
+  governorContract: string;
+  providers: string[];
+};
+
+export class Config {
+  private static chainConfigs: { [chainId: number]: ChainConfig } = {
+    1: {
+      chainId: 1,
+      governorContract: '0x2a5eEf90F1aA36CaE2535349B522891A044EFCC1',
+      providers: [
+        'https://mainnet.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161',
+        'https://eth-mainnet.public.blastapi.io',
+        'https://eth.drpc.org',
+        'https://rpc.ankr.com/eth',
+      ],
+    },
+    11155111: {
+      chainId: 11155111,
+      governorContract: '0x207bdf60d72a73d395b742D74a9E618C96Cd9a32',
+      providers: ['https://rpc.sepolia.org', 'https://sepolia.gateway.tenderly.co'],
+    },
+  };
+
+  private readonly config: ChainConfig;
+
+  public constructor(chainId: number) {
+    const config = Config.chainConfigs[chainId];
+    if (!config) {
+      throw new Error(`Unsupported chain ID: ${chainId}`);
+    }
+    this.config = config;
+  }
+
+  public get chainId(): number {
+    return this.config.chainId;
+  }
+
+  public get providers(): string[] {
+    return this.config.providers;
+  }
+
+  public governorInstance(provier: providers.Provider) {
+    return OpenZeppelinGovernanceContractFactory.connect<Governor>(
+      'Governor',
+      this.config.governorContract,
+      provier,
+    );
+  }
+}
