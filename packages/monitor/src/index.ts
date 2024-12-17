f199f3a23e2d19964a8344d038768afca524b451
ericlurker <96100006+ericlurker@users.noreply.github.com>
2024-12-17 12:33:58 +0800

    update, fix monitor error detect (#51)

--- packages/monitor/src/index.ts
+++ packages/monitor/src/index.ts
@@ -9,22 +9,27 @@ if (args.length > 0) {
 
 async function run(): Promise<number> {
   console.log(`Chain ID: ${chainId}`);
-  const config = new Config(chainId);
-  const factory = new QuorumProviderFactory();
-  const providers = factory.createProvider(config.providers);
-  const governor = config.governorInstance(providers);
+  try {
+    const config = new Config(chainId);
+    const factory = new QuorumProviderFactory();
+    const providers = factory.createProvider(config.providers);
+    const governor = config.governorInstance(providers);
 
-  const currentBlockNumber = await providers.getBlockNumber();
-  const startBlock = currentBlockNumber - 7200 - 80;
-  const endBlock = currentBlockNumber - 80;
-  const filter = governor.filters.ProposalCreated();
-  const events = await governor.queryFilter(filter, startBlock, endBlock);
-  if (events.length > 0) {
-    events.forEach((event: any) => {
-      console.log('event args', event.args);
-    });
-    console.log('!!! Warning Proposal created');
-    return 1;
+    const currentBlockNumber = await providers.getBlockNumber();
+    const startBlock = currentBlockNumber - 7200 - 80;
+    const endBlock = currentBlockNumber - 80;
+    const filter = governor.filters.ProposalCreated();
+    const events = await governor.queryFilter(filter, startBlock, endBlock);
+    if (events.length > 0) {
+      events.forEach((event: any) => {
+        console.log('event args', event.args);
+      });
+      console.log('!!! Warning Proposal created');
+      return 1;
+    }
+  } catch (e) {
+    console.error('run meet error', e);
+    return 2;
   }
   console.log('No proposal created');
   return 0;
