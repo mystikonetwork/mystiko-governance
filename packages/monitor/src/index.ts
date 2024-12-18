c1e52a807008278a41c03c847161a2e6b7789ad3
ericlurker <96100006+ericlurker@users.noreply.github.com>
2024-12-18 11:20:22 +0800

    update,  monitor provider config from mystiko config (#52)

--- packages/monitor/src/index.ts
+++ packages/monitor/src/index.ts
@@ -1,4 +1,5 @@
-import { QuorumProviderFactory } from '@mystikonetwork/utils';
+import { MystikoConfig } from '@mystikonetwork/config';
+import { DefaultProviderFactory } from '@mystikonetwork/utils';
 import { Config } from './config';
 
 const args = process.argv.slice(2);
@@ -11,11 +12,19 @@ async function run(): Promise<number> {
   console.log(`Chain ID: ${chainId}`);
   try {
     const config = new Config(chainId);
-    const factory = new QuorumProviderFactory();
-    const providers = factory.createProvider(config.providers);
+    let providerUrls: string[] = [];
+    if (chainId === 1) {
+      const mystikoConfig = await MystikoConfig.createDefaultMainnetConfig();
+      const chainConfig = mystikoConfig.getChainConfig(1);
+      providerUrls = chainConfig?.providers.map((p) => p.url) || [];
+    } else {
+      providerUrls = config.providers;
+    }
+    const factory = new DefaultProviderFactory();
+    const providers = factory.createProvider(providerUrls);
     const governor = config.governorInstance(providers);
-
     const currentBlockNumber = await providers.getBlockNumber();
+    console.log('current block number', currentBlockNumber);
     const startBlock = currentBlockNumber - 7200 - 80;
     const endBlock = currentBlockNumber - 80;
     const filter = governor.filters.ProposalCreated();
